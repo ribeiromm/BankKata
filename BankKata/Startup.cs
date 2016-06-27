a0b8da778787c92efaa18b28f57d653897eec1f9
Mario Ribeiro <ribeiromm@hotmail.com>
2016-06-27 08:44:21 +0100

    Adding code to master repository

---
+++ BankKata/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BankKata.Startup))]
+namespace BankKata
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
