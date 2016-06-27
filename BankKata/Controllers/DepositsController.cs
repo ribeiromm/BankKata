735efcb0005580b4fbd9ee4dff40e7627864be39
Mario Ribeiro <ribeiromm@hotmail.com>
2016-06-27 11:50:31 +0100

    Fist Commit of the code

---
+++ BankKata/Controllers/DepositsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using BankKata.Models;
+
+namespace BankKata.Controllers
+{
+    public class DepositsController : Controller
+    {
+        private BankKataContext db = new BankKataContext();
+
+        // GET: Deposits
+        public ActionResult Index()
+        {
+            var lines = System.IO.File.ReadAllLines(@"C: \Users\marior\Documents\visual studio 2015\Projects\BankKata\BankKata\Content\DepositSafe.txt");
+
+            var depositsList = new List<Deposit>();
+
+            foreach (var line in lines)
+            {
+                var splitLines = line.Split('|');
+                depositsList.Add(
+                    new Deposit
+                    {
+                        DepositDateTime = Convert.ToDateTime(splitLines[0]),
+                        Amount = Convert.ToDecimal(splitLines[1])
+                    });
+            }
+
+            return View(depositsList);
+        }
+
+        // GET: Deposits/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deposit deposit = db.Deposits.Find(id);
+            if (deposit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deposit);
+        }
+
+        // GET: Deposits/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Deposits/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Deposit deposit)
+        {
+            deposit.DepositDateTime = DateTime.Now;
+
+            if (ModelState.IsValid)
+            {
+                var deposits = string.Format("{0} | {1}", deposit.DepositDateTime, deposit.Amount);
+
+                using (var sw = System.IO.File.AppendText(@"C:\Users\marior\Documents\visual studio 2015\Projects\BankKata\BankKata\Content\DepositSafe.txt"))
+                {
+                    sw.WriteLine(deposits);
+                }
+
+                //db.Deposits.Add(deposit);
+                //db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(deposit);
+        }
+
+        // GET: Deposits/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deposit deposit = db.Deposits.Find(id);
+            if (deposit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deposit);
+        }
+
+        // POST: Deposits/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DepositId,Amount,DepositDateTime")] Deposit deposit)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(deposit).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(deposit);
+        }
+
+        // GET: Deposits/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Deposit deposit = db.Deposits.Find(id);
+            if (deposit == null)
+            {
+                return HttpNotFound();
+            }
+            return View(deposit);
+        }
+
+        // POST: Deposits/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Deposit deposit = db.Deposits.Find(id);
+            db.Deposits.Remove(deposit);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BankKata/Models/BankKataContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace BankKata.Models
+{
+    public class BankKataContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public BankKataContext() : base("name=BankKataContext")
+        {
+        }
+
+        public DbSet<Deposit> Deposits { get; set; }
+    }
+}
