940f2e48a198627a2b7e772241855395b9e6e4e4
ribeiromm <ribeiromm@hotmail.com>
2016-07-25 08:27:45 +0100

    Adding call to web api

---
+++ BankKata/Models/Investiment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankKata.Models
+{
+    public class Investiment
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string SharePrice { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime TimeRequested { get; set; }
+    }
+}
